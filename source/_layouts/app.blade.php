15594968affefe644adde71c0d10922640304f20
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-06-21 21:01:37 -0700

    Load stylesheet before javascript because I like it better that way

--- source/_layouts/app.blade.php
+++ source/_layouts/app.blade.php
@@ -3,8 +3,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
-<script src="{{ mix('js/app.js', 'assets/build') }}" defer></script>
 <link href="{{ mix('css/app.css', 'assets/build') }}" rel="stylesheet">
+<script src="{{ mix('js/app.js', 'assets/build') }}" defer></script>
 
 <title>PHX2600 • The Phoenix, Arizona Network of Hackers</title>
 
