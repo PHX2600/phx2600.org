18220e4352c450eff42fc1c8a18f5c44d9f4207d
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-07-25 22:21:32 -0700

    Enabled PurgeCSS when building in prod

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
 let tailwindcss = require('tailwindcss');
+require('laravel-mix-purgecss');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
@@ -31,3 +32,7 @@ mix.copyDirectory(
     'node_modules/@fortawesome/fontawesome-free/webfonts',
     'source/assets/webfonts'
 );
+
+mix.purgeCss({
+    folders: ['source']
+});
